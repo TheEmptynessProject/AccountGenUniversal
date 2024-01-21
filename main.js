1be941d337411973d439131919f926441551058c
Emptyness <89524326+TheEmptynessProject@users.noreply.github.com>
2024-01-21 02:24:21 +0000

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,21 @@
 // @name         AccountGenUniversal
 // @version      2.0
 // @license      MIT
-// @description  Wondering how to use? Check out the readme down here \/
+// @description  Are you tired of spam emails? Worried about your privacy? Well... You're welcome!
 // @namespace    https://github.com/TheEmptynessProject/AccountGenUniversal
 // @author       TheEmptynessProject (https://github.com/TheEmptynessProject)
 // @match        *://*/*
 // @grant        GM_setClipboard
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
     let passLen = 16; //Set to your desired password length
     const SwalScript = document.createElement('script');
     SwalScript.src = 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
     document.head.appendChild(SwalScript);
-
+ 
     window.addEventListener('load', () => {
         new Promise((resolve) => {
             SwalScript.onload = resolve;
@@ -28,23 +28,23 @@
                 const symbols = "\|!@#%&/([)]=}?+*~^,.:-_<>";
                 const all = lowerLetters + upperLetters + numbers + symbols;
                 let pass = "";
-
+ 
                 pass += lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
                 pass += upperLetters[Math.floor(Math.random() * upperLetters.length)];
                 pass += numbers[Math.floor(Math.random() * numbers.length)];
                 pass += symbols[Math.floor(Math.random() * symbols.length)];
-
+ 
                 for (let i = 4; i < leng; i++) {
                     pass += all[Math.floor(Math.random() * all.length)];
                 }
-
+ 
                 pass = pass.split('').sort(function() {
                     return 0.5 - Math.random()
                 }).join('');
-
+ 
                 return pass;
             }
-
+ 
             function getEmail() {
                 return new Promise((resolve, reject) => {
                     GM_xmlhttpRequest({
@@ -57,7 +57,7 @@
                     });
                 });
             }
-
+ 
             function waitForEmail(email) {
                 const intervalId2 = setInterval(function() {
                     GM_xmlhttpRequest({
@@ -65,16 +65,16 @@
                         url: `https://www.1secmail.com/api/v1/?action=getMessages&login=${email.substring(0, email.indexOf('@'))}&domain=${email.substring(email.indexOf('@') + 1)}`,
                         onload: function(response) {
                             const ref_response = JSON.parse(response.responseText);
-
+ 
                             if (ref_response.length > 0) {
                                 const first_msg = ref_response[0];
                                 const msg_id = first_msg.id;
                                 const from_msg = first_msg.from || 'Unknown Sender';
                                 const subject = first_msg.subject || 'No Subject';
                                 const date = first_msg.date || 'No Date';
-
+ 
                                 const msg_details = `From: ${from_msg}\nSubject: ${subject}`;
-
+ 
                                 GM_xmlhttpRequest({
                                     method: "GET",
                                     url: `https://www.1secmail.com/api/v1/?action=readMessage&login=${email.substring(0, email.indexOf('@'))}&domain=${email.substring(email.indexOf('@') + 1)}&id=${msg_id}`,
@@ -91,7 +91,7 @@
                     });
                 }, 1000);
             }
-
+ 
             function openEmailInNewTab(content) {
                 const dataUri = 'data:text/html,' + encodeURIComponent(content);
                 const newTab = window.open(dataUri, '_blank');
@@ -101,7 +101,7 @@
                     console.log(content);
                 }
             }
-
+ 
             async function generate() {
                 let email = await getEmail();
                 let pass = generatePassword(passLen);
@@ -126,7 +126,7 @@
                 waitForEmail(email);
             }
             generate();
-
+ 
         })
     });
 })();
